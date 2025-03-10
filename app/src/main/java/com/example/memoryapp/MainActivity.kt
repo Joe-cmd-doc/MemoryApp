6cee9c8e8f598749657b2bcd43f932a2fa017d19
Joe-cmd-doc <joelortizrivas@gmail.com>
2025-03-10 18:01:24 +0100

    menu hecho

--- app/src/main/java/com/example/memoryapp/MainActivity.kt
+++ app/src/main/java/com/example/memoryapp/MainActivity.kt
@@ -1,23 +1,57 @@
 package com.example.memoryapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    var user: FirebaseUser? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Instanciem el firebaseAuth
+        //assigna valor a user
         auth = FirebaseAuth.getInstance()
+        user = auth.currentUser
+        53
+        var BTMLOGIN = findViewById<Button>(R.id.BTMLOGIN);
+        var BTMREGISTRO = findViewById<Button>(R.id.BTMREGISTRO);
+        BTMLOGIN.setOnClickListener() {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+        BTMREGISTRO.setOnClickListener() {
 
+            val intent = Intent(this, Registro::class.java)
+            startActivity(intent)
+
+        }
     }
+
+    // Aquest mètode s'executarà quan s'obri el menu
+    override fun onStart() {
+        usuariLogejat()
+        super.onStart()
+    }
+    private fun usuariLogejat() {
+        if (user !=null)
+        {
+            val intent= Intent(this, Menu::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+
+
 
     fun fesuntoast(view: View){
         Toast.makeText(this, "Has picat el botó!", Toast.LENGTH_SHORT).show()
@@ -45,5 +79,7 @@ class MainActivity : AppCompatActivity() {
             }
 
     }
+
+
 
 }
---
+++ app/src/main/java/com/example/memoryapp/Menu.kt
@@ -0,0 +1,112 @@
+package com.example.memoryapp
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class Menu : AppCompatActivity() {
+    //creem unes variables per comprovar ususari i authentificació
+    lateinit var auth: FirebaseAuth
+    var user: FirebaseUser? = null;
+    lateinit var tancarSessio: Button
+    lateinit var CreditsBtn: Button
+    lateinit var PuntuacionsBtn: Button
+    lateinit var jugarBtn: Button
+
+    lateinit var miPuntuaciotxt: TextView
+    lateinit var puntuacio: TextView
+    lateinit var uid: TextView
+    lateinit var correo: TextView
+    lateinit var nom: TextView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        auth= FirebaseAuth.getInstance()
+        user =auth.currentUser
+
+        tancarSessio =findViewById<Button>(R.id.tancarSessio)
+        CreditsBtn =findViewById<Button>(R.id.CreditsBtn)
+        PuntuacionsBtn =findViewById<Button>(R.id.PuntuacionsBtn)
+        jugarBtn =findViewById<Button>(R.id.jugarBtn)
+
+        //Aquí creem un tipus de lletra a partir de una font
+        val tf = Typeface.createFromAsset(assets,"fonts/comicate.TTF")
+        miPuntuaciotxt=findViewById(R.id.miPuntuaciotxt)
+        puntuacio=findViewById(R.id.puntuacio)
+        uid=findViewById(R.id.uid)
+        correo=findViewById(R.id.correo)
+        nom=findViewById(R.id.nom)
+
+        //els hi assignem el tipus de lletra
+        miPuntuaciotxt.setTypeface(tf)
+        puntuacio.setTypeface(tf)
+        uid.setTypeface(tf)
+        correo.setTypeface(tf)
+        nom.setTypeface(tf)
+
+        //fem el mateix amb el text dels botons
+        tancarSessio.setTypeface(tf)
+        CreditsBtn.setTypeface(tf)
+        PuntuacionsBtn.setTypeface(tf)
+        jugarBtn.setTypeface(tf)
+
+
+        tancarSessio.setOnClickListener(){
+            tancalaSessio()
+        }
+        CreditsBtn.setOnClickListener(){
+            Toast.makeText(this,"Credits", Toast.LENGTH_SHORT).show()
+        }
+        PuntuacionsBtn.setOnClickListener(){
+            Toast.makeText(this,"Puntuacions", Toast.LENGTH_SHORT).show()
+        }
+        jugarBtn.setOnClickListener(){
+            Toast.makeText(this,"JUGAR", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun Usuarilogejat()
+    {
+        if (user !=null)
+        {
+            Toast.makeText(this,"Jugador logejat",
+                Toast.LENGTH_SHORT).show()
+        }
+        else
+        {
+            val intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    // Aquest mètode s'executarà quan s'obri el minijoc
+    override fun onStart() {
+        Usuarilogejat()
+        super.onStart()
+    }
+
+
+
+    private fun tancalaSessio() {
+        auth.signOut() //tanca la sessió
+        //va a la pantalla inicial
+        val intent= Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
